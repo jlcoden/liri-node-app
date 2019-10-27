af58b4b3889164dd3417b88370062bf8f870553a
Josh Cosson <joshlcosson@gmail.com>
2019-10-27 14:41:04 -0700

    Created files for Liri Bot application, created functions for command line and for concert-this command

---
+++ liri.js
@@ -0,0 +1,78 @@
+//require("dotenv").config();
+//var keys = require("./keys.js");
+
+//var spotify = new Spotify(keys.spotify);
+
+var axios = require("axios");
+let userInput = process.argv[2];
+let userQuery = process.argv.slice(3).join("+");
+
+function userCommand(userInput, userQuery) {
+  //make a decidsion based on the user input in the command
+  switch (userInput) {
+    case "concert-this":
+      concertThis();
+      break;
+    // case "spotify-this":
+    //    spotifyThis();
+    //     break;
+    // case "movie-this":
+    //     movieThis();
+    //     break;
+    // default:
+    // console.log(" I don't understand the request");
+    // break;
+  }
+}
+userCommand(userInput, userQuery);
+
+function concertThis() {
+  var queryUrl =
+    "https://rest.bandsintown.com/artists/" +
+    userQuery +
+    "/events?app_id=codingbootcamp";
+
+  console.log(userQuery);
+  console.log(userInput);
+  console.log(queryUrl);
+
+  axios
+    .get(queryUrl)
+    .then(function(response) {
+      console.log("Venue Name: " + response.data[0].venue.name);
+      console.log(
+        "Venue location: " +
+          " Latitude:" +
+          response.data[0].venue.latitude +
+          ", Longitude: " +
+          response.data[0].venue.longitude +
+          ", City:" +
+          response.data[0].venue.city +
+          ", Region:" +
+          response.data[0].venue.region +
+          ", Country: " +
+          response.data[0].venue.country
+      );
+      console.log("Date of Event: ");
+    })
+    .catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+}
