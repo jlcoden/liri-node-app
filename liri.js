570b843bec9c18e9815f0bc0feee50a9dfbf5725
Josh Cosson <joshlcosson@gmail.com>
2019-10-27 17:26:28 -0700

    Added movieThis function to the App, so users can search for movies

--- liri.js
+++ liri.js
@@ -14,9 +14,9 @@ function userCommand(userInput, userQuery) {
     case "spotify-this":
       spotifyThis();
       break;
-    // case "movie-this":
-    //     movieThis();
-    //     break;
+    case "movie-this":
+      movieThis();
+      break;
     // default:
     // console.log(" I don't understand the request");
     // break;
@@ -101,4 +101,55 @@ function spotifyThis() {
       }
     }
   );
+}
+
+function movieThis() {
+  if (!userQuery) {
+    userQuery = "Mr. Nobody";
+  }
+
+  var queryUrl =
+    "http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=trilogy";
+
+  axios
+    .get(queryUrl)
+    .then(function(response) {
+      //Title of the movie
+      console.log("Movie Title: " + response.data.Title);
+      // // * Year the movie came out
+      console.log("Release Year: " + response.data.Year);
+      // * IMDB Rating of the movie
+      console.log("IMDB Rating: " + response.data.imdbRating);
+      // * Rotten Tomatoes Rating of the movie
+      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[2].Value);
+      // * Country where the movie was produced
+      console.log("Country Produced In: " + response.data.Country);
+      // * Language of the movie
+      console.log("Language: " + response.data.Language);
+      // * Plot of the movie
+      console.log("Plot: " + response.data.Plot);
+      // * Actors in movie
+      console.log("Actors: " + response.data.Actors);
+    })
+
+    .catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
 }
